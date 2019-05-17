994c1ce3ab610342437f0215c049d60d15c7320a
bkushigian <bkushigian@gmail.com>
2019-05-17 15:50:29 -0400

    Created some beans

--- src/state.py
+++ src/state.py
@@ -34,5 +34,14 @@ class Game:
         self.discard = []
 
 
-Card(name="Coffee Bean", exchange=(4, 7, 10, 12), count=24)
-
+Card(name="Coffee Bean",        exchange=(4,    7, 10, 12),    count=24)
+Card(name="Wax Bean",           exchange=(4,    7,  9, 11),    count=22)
+Card(name="Blue Bean",          exchange=(4,    6,  8, 10),    count=20)
+Card(name="Chili Bean",         exchange=(3,    6,  8,  9),    count=18)
+Card(name="Stink Bean",         exchange=(3,    5,  7,  8),    count=16)
+Card(name="Green Bean",         exchange=(3,    5,  6,  7),    count=14)
+Card(name="Soy Bean",           exchange=(2,    4,  6,  7),    count=12)
+Card(name="Black-eyed Bean",    exchange=(2,    4,  5,  6),    count=10)
+Card(name="Red Bean",           exchange=(2,    3,  4,  5),    count=8)
+Card(name="Garden Bean",        exchange=(None, 2,  3,  None), count=6)
+Card(name="Cocoa Bean",         exchange=(None, 2,  3,  4),    count=4)
